bd7b64ab1c03bc0c108f0ce5fe0a3a94469817e5
LAPTOP-5GJN3M32\AbdulRahman <abdulraxman.basha@gmail.com>
2018-01-23 17:57:15 +0300

    Added another action with a extra x

--- WebApplication11/Controllers/HomeController.cs
+++ WebApplication11/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace WebApplication11.Controllers
             return View();
         }
 
+        public ActionResult Indexx()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
